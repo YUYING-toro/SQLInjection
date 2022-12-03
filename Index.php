3c8d130b5fdfca731c3f009e1baf2cc463709280
YUYING-toro <55909729+YUYING-toro@users.noreply.github.com>
2022-12-03 10:56:08 -0500

    set Up

---
+++ Index.php
@@ -0,0 +1,16 @@
+<?php
+include("Common/Header.php");
+?>
+<script type="text/javascript" src="jq.js"></script>
+<body> 
+    <div class="container"> 
+
+       <script type="text/javascript">
+
+       </script>
+    </div>
+</body>
+
+<?php
+include("Common/Footer.php");
+?>
