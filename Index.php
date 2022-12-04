4023c4c188d11c76052401fb283d20b95960ed55
Karlos Ye <58801593+karlosye@users.noreply.github.com>
2022-12-04 14:29:20 -0500

    Update Index.php

--- Index.php
+++ Index.php
@@ -1,6 +1,64 @@
 <?php
 include("Common/Header.php");
 ?>
+
+<?php
+
+$accountInput = $_POST['account'];
+$passwdInput = $_POST['password'];
+
+// implement php code to prevent SQL injection: use PDO - PHP Data Object
+
+if (isset($accountInput)) {
+
+    //validate if $accountInput contains any special character
+    if (!preg_match('/[\'^£$%&*()}{@#~?><>,|=_+¬-]/', $accountInput)) {
+
+        try {
+
+            $dbh = new PDO('mysql:host=localhost;dbname=sql_injection_example', 'user', 'password');
+
+            $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            // prepare the SQL statement
+            $q = "SELECT username 
+                FROM users
+                WHERE id = :id";
+            // Prepare the SQL query string.
+            $sth = $dbh->prepare($q);
+            // Bind parameters to statement variables.
+            $sth->bindParam(':id', $id);
+            // Execute statement.
+            $sth->execute();
+            // Set fetch mode to FETCH_ASSOC to return an array indexed by column name.
+            $sth->setFetchMode(PDO::FETCH_ASSOC);
+            // Fetch result.
+            $result = $sth->fetchColumn();
+            /**
+             * HTML encode our result using htmlentities() to prevent stored XSS and print the
+             * result to the page
+             */
+            print(htmlentities($result));
+
+            //Close the connection to the database.
+            $dbh = null;
+        } catch (PDOException $e) {
+
+            error_log('PDOException - ' . $e->getMessage(), 0);
+
+            http_response_code(500);
+            die('Error establishing connection with database');
+        }
+    } else {
+
+        // return this message if there's issue with the input string
+        http_response_code(400);
+        die('Error processing bad or malformed request');
+    }
+}
+
+?>
+
 <script type="text/javascript" src="jq.js"></script>
 
 <body>
@@ -12,12 +70,12 @@ include("Common/Header.php");
                     <table>
                         <tr>
                             <th>Account</th>
-                            <td><input type="text" name="account" value="<?php print_r($account); ?>" class="account" id="account"></td>
+                            <td><input type="text" name="account" class="account" id="account"></td>
 
                         </tr>
                         <tr>
                             <th>Password</th>
-                            <td><input type="password" name="password" value="<?php print_r($password); ?>" class="password" id="password"></td>
+                            <td><input type="text" name="password" class="password" id="password"></td>
                         </tr>
                         <tr>
 
@@ -25,7 +83,9 @@ include("Common/Header.php");
                                 <input type="submit" name="submit" value="submit" class="btn-primary btn" style="margin-right: 1.5rem ">
                                 <input type="reset" name="reset" value="Reset" class="btn-primary btn" onclick="clearInput()">
                             </td>
-                            <td> </td>
+                            <td>
+                                <small style="display: none; color:red" id="errorMessage">The input cannot contain any special characters</small>
+                            </td>
                         </tr>
                     </table>
                 </form>
@@ -45,20 +105,34 @@ include("Common/Header.php");
 
     function clearInput() {
         accountInput.innerHTML = "";
-        passwdInput.innerHTML = "";
+        passwdInput.innerHTML = "sdfsdf";
     }
 
     // Evenlistener when the submit btn is clicked: see if it contains any special characters
     let form = document.getElementById("Form");
 
+    function containsSpecialChars(str) {
+        const specialChars = `\`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~`;
+
+        const result = specialChars.split('').some(specialChar => {
+            if (str.includes(specialChar)) {
+                return true;
+            }
+
+            return false;
+        });
+
+        return result;
+    }
+
     form.addEventListener('submit', function(event) {
 
-        let format = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
+        console.log("Submit btn clicked");
 
-        if (!format.test(accountInput.innerText) || !format.test(passwdInput.innerText)) {
+        if (containsSpecialChars(accountInput.innerText) || containsSpecialChars(passwdInput.innerText)) {
 
-            // prevent the form from submitting if it contains any special charcters
             event.preventDefault();
+            document.getElementById("errorMessage").style.display = "inline";
         }
     })
 </script>
